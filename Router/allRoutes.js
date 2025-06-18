35ab28c43eed840f9b7263447a2f97dba4f80112
Este <et.thomas@hotmail.fr>
2025-06-18 15:44:51 +0200

    Galerie+Connexion+Inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,11 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/Accueil", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/Connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/Inscription", "Inscription", "/pages/inscription.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
