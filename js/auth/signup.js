a233afc1f3e7351804a43b3107e8713ca7dbfa19
EstebanThomas <et.thomas@hotmail.fr>
2025-06-20 17:04:20 +0200

    Validation js inscription

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -15,8 +15,10 @@ function validateForm(){
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPreNom);
     const emailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
+    const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword)
 
-    if(nomOk && prenomOk && emailOk){
+    if(nomOk && prenomOk && emailOk && passwordOk && passwordConfirmOk){
         btnValidation.disabled = false;
     }
     else{
@@ -48,6 +50,34 @@ function validateMail(input){
     else{
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validatePassword(input){
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if(inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
         return false;
     }
 }
